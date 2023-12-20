a9d0faa9b0b3f681c4f29e4c1b8f0eec30d522c6
Carlos Moreno <carlos.moreno@endrock.software>
2023-12-19 23:22:47 -0500

    send day 2
    day 2

--- assets/global.js
+++ assets/global.js
@@ -1269,3 +1269,17 @@ class ProductRecommendations extends HTMLElement {
 }
 
 customElements.define('product-recommendations', ProductRecommendations);
+
+
+
+/* Agregamos efecto zoom +_-  */
+
+
+function zoom(e){
+  var zoomer = e.currentTarget;
+  e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+  e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+  x = offsetX/zoomer.offsetWidth*100
+  y = offsetY/zoomer.offsetHeight*100
+  zoomer.style.backgroundPosition = x + '% ' + y + '%';
+}
